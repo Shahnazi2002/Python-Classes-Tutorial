c6488ef9fa0fa98d887591716a753da866fc4e8b
Mohammad SN <shahnazi2002@gmail.com>
2022-06-29 13:15:20 +0430

    Update class.py

--- class.py
+++ class.py
@@ -8,8 +8,8 @@ class Person:
 
 
 p1 = Person("Mohammad Shahnazi", 20)
-print("Name:", p1.name)                                                          # Name: Mohammad Shahnazi
-print("Age:", p1.age)                                                            # Age: 20
+print("Name:", p1.name)                                                                                 # Name: Mohammad Shahnazi
+print("Age:", p1.age)                                                                                   # Age: 20
 
 
 print("\n------------------------\n")
@@ -28,8 +28,8 @@ class Person:
 
 p1 = Person("Mohammad", "Shahnazi", 20)
 p2 = Person("Reza", "Barghi", 32)
-p1.full_name()                                                                   # Mohammad Shahnazi
-p2.full_name()                                                                   # Reza Barghi
+p1.full_name()                                                                                          # Mohammad Shahnazi
+p2.full_name()                                                                                          # Reza Barghi
 
 
 print("\n------------------------\n")
@@ -60,10 +60,30 @@ class Teacher(Person):
 
 student1 = Student("Mohammad", "Shahnazi")
 teacher1 = Teacher("Alireza", "Ghaffari")
-student1.full_name()                                                             # Mohammad Shahnazi
-student1.info()                                                                  # I am a student. My name is Mohammad Shahnazi
-teacher1.full_name()                                                             # Alireza Ghaffari
-teacher1.info()                                                                  # I am a teacher. My name is Alireza Ghaffari
+student1.full_name()                                                                                    # Mohammad Shahnazi
+student1.info()                                                                                         # I am a student. My name is Mohammad Shahnazi
+teacher1.full_name()                                                                                    # Alireza Ghaffari
+teacher1.info()                                                                                         # I am a teacher. My name is Alireza Ghaffari
+
+
+print("\n------------------------\n")
+
+
+class Member:
+    def __init__(self, id, first_name, last_name, birthyear):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birthyear = birthyear
+
+    def __str__(self):
+        return f"ID: {self.id}\nName: {self.first_name} {self.last_name}\nAge: {2022-self.birthyear}"
+
+
+m1 = Member(1001, "Mohammad", "Shahnazi", 2002)
+print(m1)                                                                                               # ID: 1001
+                                                                                                        # Name: Mohammad Shahnazi
+                                                                                                        # Age: 20
 
 
 print("\n------------------------\n")
