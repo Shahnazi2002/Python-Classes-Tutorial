fa81f13277e97d3656c88a179b65fded95310776
Mohammad SN <shahnazi2002@gmail.com>
2022-06-29 12:14:58 +0430

    Update class.py

--- class.py
+++ class.py
@@ -6,9 +6,10 @@ class Person:
         self.age = age
 
 
+
 p1 = Person("Mohammad Shahnazi", 20)
-print("Name:", p1.name)                              # Name: Mohammad Shahnazi
-print("Age:", p1.age)                                # Age: 20
+print("Name:", p1.name)                                                          # Name: Mohammad Shahnazi
+print("Age:", p1.age)                                                            # Age: 20
 
 
 print("\n------------------------\n")
@@ -22,29 +23,47 @@ class Person:
 
     def full_name(self):
         print(self.first_name, self.last_name)
+
 
 
 p1 = Person("Mohammad", "Shahnazi", 20)
-p2 = Person("Reza", "Hashemi", 32)
-p1.full_name()                                       # Mohammad Shahnazi
-p2.full_name()                                       # Reza Hashemi
+p2 = Person("Reza", "Barghi", 32)
+p1.full_name()                                                                   # Mohammad Shahnazi
+p2.full_name()                                                                   # Reza Barghi
 
 
 print("\n------------------------\n")
 
 
 class Person:
-    def __init__(self, first_name, last_name, age):
+    def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
-        self.age = age
 
     def full_name(self):
         print(self.first_name, self.last_name)
 
+    def info(self):
+        print("I am", self.first_name, self.last_name)
+
 
 class Student(Person):
-    pass
+    def info(self):
+        print("I am a student. My name is", self.first_name, self.last_name)
 
 
-# June 2022
+class Teacher(Person):
+    def info(self):
+        print("I am a teacher. My name is", self.first_name, self.last_name)
+
+
+
+student1 = Student("Mohammad", "Shahnazi")
+teacher1 = Teacher("Alireza", "Ghaffari")
+student1.full_name()                                                             # Mohammad Shahnazi
+student1.info()                                                                  # I am a student. My name is Mohammad Shahnazi
+teacher1.full_name()                                                             # Alireza Ghaffari
+teacher1.info()                                                                  # I am a teacher. My name is Alireza Ghaffari
+
+
+print("\n------------------------\n")
